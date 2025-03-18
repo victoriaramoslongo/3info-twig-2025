c596c136727a19d13585b0de68ac5dfa8e67d5bc
victoriaramoslongo <vitoriadosanjos567@gmail.com>
2025-03-18 08:49:39 -0300

    comiit
    miau

--- compras.php
+++ compras.php
@@ -1,7 +1,16 @@
 <?php
 
 require_once('twig_carregar.php');
+require('inc/banco.php');
+
+//busca as compras no banco 
+$dados = $pdo->query('SELECT * FROM compras'); 
+
+$comp = $dados->fetchAll(PDO::FETCH_ASSOC);
+
 
 echo $twig->render('compras.html' , [
     'titulo' => 'Compras' , 
+    'compras' => $comp, 
+
 ]);
